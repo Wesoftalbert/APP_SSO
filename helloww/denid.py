3a93150797306d6c6ec512af1af63c34bbd1d561
albert.chen <15296809123@163.com>
2015-11-02 12:02:44 +0800

    change

---
+++ helloww/denid.py
@@ -0,0 +1,14 @@
+'''
+Created on Nov 2, 2015
+
+@author: Administrator
+'''
+
+print "hello world"
+print "hello world"
+print "hello world"
+print "hello world"
+print "hello world"
+print "hello world"
+print "hello world"
+print "hello world"
